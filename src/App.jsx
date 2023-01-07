db4f4b73bbe7843e82816f99289a03f2782a5ec4
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2023-01-07 14:10:39 +0530

    start commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from './Components/Navbar'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+        <div className='nav'>
+            <div>
+                <img src='https://www.masaischool.com/img/navbar/logo.svg' alt='logo'/>
+            </div>
+            <p>COURSES</p>
+            <p>REFER & EARN</p>
+            <p>FEES & PAP</p>
+            <p>FEES RESOURCES</p>
+            <p>FOR CORPORATES</p>
+            <button>FOR STUDENTS</button>
+            <button>LOGIN</button>
+        </div>
+  )
+}
